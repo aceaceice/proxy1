253bb07ebafeeca4dded837614c3090c4354560f
Yaroslav <prostowoy@gmail.com>
2020-06-07 12:56:37 +0300

    port

--- index.js
+++ index.js
@@ -43,4 +43,6 @@ const executeRequest = (options, clientRequest, clientResponse) => {
 };
 const server = http.createServer(parseIncomingRequest);
 const PORT = process.env.PORT || 5000;
-server.listen(PORT);
+server.listen(PORT, () => {
+  console.log(PORT);
+});
