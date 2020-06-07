1dd53c69f050c0b5c552da780cbaebf1406c7621
Yaroslav <prostowoy@gmail.com>
2020-06-07 21:40:06 +0300

    qw

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { logger } = require("./utils");
 const { allowedFormat } = require("./plugins");
 const { check } = require("./plugins");
 const auth = require("basic-auth");
+const { proxyCheck } = require("./plugins");
+
 const parseIncomingRequest = (clientRequest, clientResponse) => {
   const { host, port, path } = url.parse(clientRequest.url);
   const credentials = auth(clientRequest);
@@ -18,12 +20,14 @@ const parseIncomingRequest = (clientRequest, clientResponse) => {
   };
   if (!credentials || !check(credentials.name, credentials.pass)) {
     clientResponse.statusCode = 401;
-    clientResponse.setHeader("WWW-Authenticate", 'Basic realm="example"');
+    clientResponse.setHeader(
+      "WWW-Authenticate",
+      'Basic realm="Access to the internal site"'
+    );
     clientResponse.end("Access denied");
   } else {
     options.allowed =
-      !blockedResources(options, allowedFormat) &&
-      Object.prototype.hasOwnProperty.call(options.headers, "proxy-connection");
+      !blockedResources(options, allowedFormat) && proxyCheck(options.headers);
     logger(options);
     options.allowed
       ? executeRequest(options, clientRequest, clientResponse)
